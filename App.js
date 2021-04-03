78a1c9229c3fd409b73f558d5a2bdea9ddee49f2
abbuyanaa <abbuyanaa@gmail.com>
2021-04-03 09:59:14 +0800

    Added SQLite CRUD functions on db.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
@@ -6,10 +6,32 @@ import PlacesListScreen from "./src/screen/PlacesListScreen";
 import NewPlaceScreen from "./src/screen/NewPlaceScreen";
 import PlaceDetailScreen from "./src/screen/PlaceDetailScreen";
 import MapScreen from "./src/screen/MapScreen";
+import { initDB, insertPlace, getPlaces, clearPlaces } from "./src/helpers/db";
 
 const Stack = createStackNavigator();
 
 function App() {
+  useEffect(() => {
+    initDB()
+      .then((result) => {
+        (async () => {
+          console.log("Өгөгдлүүдийг баазд хийх гэж байна...");
+          await insertPlace("Кофе", "file://hello.jpg", "хаяг", 23.12, -33.233);
+          await insertPlace(
+            "Бассейн",
+            "file://bassein.jpg",
+            "хаяг",
+            33.12,
+            -22.233
+          );
+          const result = await getPlaces();
+          console.log("Үр дүн: ", result.rows._array);
+          await clearPlaces();
+        })();
+        console.log("Баазыг бэлтгэж дууслаа...");
+      })
+      .catch((err) => console.log("Баазыг бэлтгэхэд асуудал гарлаа!", err));
+  }, []);
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Places">
