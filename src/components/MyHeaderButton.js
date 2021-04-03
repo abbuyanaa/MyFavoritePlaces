b9565034af2374314f8043d18d3ecea5b9e1cb3c
abbuyanaa <abbuyanaa@gmail.com>
2021-04-03 10:06:51 +0800

    Added MyHeaderButton.js file

---
+++ src/components/MyHeaderButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { HeaderButton } from "react-navigation-header-buttons";
+
+const MyHeaderButton = () => {
+  return <HeaderButton></HeaderButton>;
+};
+
+export default MyHeaderButton;
+
+const styles = StyleSheet.create({});
